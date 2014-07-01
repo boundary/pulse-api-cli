c5b802c6d4bbe11055050da7d09221a70d749364
jdgwartney <davidg@boundary.com>
2014-07-01 13:23:32 -0700

    Starting point

---
+++ src/main/java/com/boundary/App.java
@@ -0,0 +1,13 @@
+package com.boundary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
