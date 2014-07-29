28cc48817f85a7d112fb17e3fb7c889a84569247
jdgwartney <davidg@boundary.com>
2014-07-29 11:04:24 -0700

    Remove suffix from shell scripts

--- env.sh
+++ env.sh
@@ -1,1 +1,6 @@
 export BOUNDARY_API_SHELL_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export PATH="$PATH:$BOUNDARY_API_SHELL_HOME/bin"
+export PATH="$PATH:$BOUNDARY_API_SHELL_HOME/src/main/scripts/meter"
+export PATH="$PATH:$BOUNDARY_API_SHELL_HOME/src/main/scripts/metrics"
+
