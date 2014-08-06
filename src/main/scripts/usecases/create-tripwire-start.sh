8886a6013f4bb00ad829c2db33b28cb51dd2015b
jdgwartney <davidg@boundary.com>
2014-08-05 18:54:45 -0700

    Working versions of rules and scripts

---
+++ src/main/scripts/usecases/create-tripwire-start.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+curl -X POST http://localhost:8080/ChMBk5Ou9QBdDPaTai0rfUbZ8bC/triangulation/rules -d @"$(dirname $SCRIPT_PATH)/rules/tripwire-start.json" -H "Content-Type: application/json" --verbose
