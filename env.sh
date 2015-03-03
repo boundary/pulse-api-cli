5de4af08b3d5ad42ba157944b0b2395a03fa0e8b
jdgwartney <davidg@boundary.com>
2015-03-03 11:27:21 -0800

    Remove adding local bin to the PATH

--- env.sh
+++ env.sh
@@ -16,7 +16,6 @@
 
 export BOUNDARY_API_SHELL_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export PATH="$PATH:$BOUNDARY_API_SHELL_HOME/bin"
 export PATH="$PATH:$BOUNDARY_API_SHELL_HOME/src/main/scripts/account"
 export PATH="$PATH:$BOUNDARY_API_SHELL_HOME/src/main/scripts/actions"
 export PATH="$PATH:$BOUNDARY_API_SHELL_HOME/src/main/scripts/events"
