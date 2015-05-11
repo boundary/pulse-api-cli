203707235c902a0fdcba64a3f34c7bb3b6ac7d07
jdgwartney <davidg@boundary.com>
2015-05-11 11:18:02 -0700

    format code

--- boundary/metric_create.py
+++ boundary/metric_create.py
@@ -1,18 +1,18 @@
-###
-### Copyright 2014-2015 Boundary, Inc.
-###
-### Licensed under the Apache License, Version 2.0 (the "License");
-### you may not use this file except in compliance with the License.
-### You may obtain a copy of the License at
-###
-###     http://www.apache.org/licenses/LICENSE-2.0
-###
-### Unless required by applicable law or agreed to in writing, software
-### distributed under the License is distributed on an "AS IS" BASIS,
-### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-### See the License for the specific language governing permissions and
-### limitations under the License.
-###
+#
+# Copyright 2014-2015 Boundary, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 from api_cli import ApiCli
 
 class MetricCreate (ApiCli):
@@ -32,14 +32,14 @@ class MetricCreate (ApiCli):
         
     def addArguments(self):
         ApiCli.addArguments(self)
-        self.parser.add_argument('-n', '--metric-name', dest='metricName',action='store',required=True,metavar='metric_name',help='Metric identifier')
-        self.parser.add_argument('-d', '--display-name', dest='displayName',action='store',metavar='display_name',help='Metric display name')
-        self.parser.add_argument('-s', '--display-name-short', dest='displayNameShort',action='store',metavar='display_short_name',help='Metric short display name')
-        self.parser.add_argument('-i', '--description', dest='description',action='store',metavar='description',help='Metric description')
-        self.parser.add_argument('-g', '--aggregate', dest='aggregate',action='store',choices=['sum','avg','max','min'],help='Metric default aggregate')
-        self.parser.add_argument('-u', '--unit', dest='unit',action='store',choices=['percent','number','bytecount','duration'],help='Metric unit')
-        self.parser.add_argument('-r', '--resolution', dest='resolution',action='store',metavar='resolution',help='Metric default resolution')
-        self.parser.add_argument('-x', '--is-disabled',dest='isDisabled',action='store_true',help='disable metric')
+        self.parser.add_argument('-n', '--metric-name', dest='metricName', action='store', required=True, metavar='metric_name', help='Metric identifier')
+        self.parser.add_argument('-d', '--display-name', dest='displayName', action='store', metavar='display_name', help='Metric display name')
+        self.parser.add_argument('-s', '--display-name-short', dest='displayNameShort', action='store', metavar='display_short_name', help='Metric short display name')
+        self.parser.add_argument('-i', '--description', dest='description', action='store', metavar='description', help='Metric description')
+        self.parser.add_argument('-g', '--aggregate', dest='aggregate', action='store', choices=['sum', 'avg', 'max', 'min'], help='Metric default aggregate')
+        self.parser.add_argument('-u', '--unit', dest='unit', action='store', choices=['percent', 'number', 'bytecount', 'duration'], help='Metric unit')
+        self.parser.add_argument('-r', '--resolution', dest='resolution', action='store', metavar='resolution', help='Metric default resolution')
+        self.parser.add_argument('-x', '--is-disabled', dest='isDisabled', action='store_true', help='disable metric')
         
     def getArguments(self):
         '''
