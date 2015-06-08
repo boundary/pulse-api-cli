30e4933a9e49928f698dafa4f38a9b5eefa32c7b
jdgwartney <davidg@boundary.com>
2015-06-08 09:24:38 -0700

    Change imports to include boundary module only

--- boundary/metric_modify.py
+++ boundary/metric_modify.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from boundary.metric_common import MetricCommon
+from boundary import MetricCommon
 import json
 
 """
