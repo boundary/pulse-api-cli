57aa6a516716c881aa06b902f2bd76a4970a0abc
jdgwartney <davidg@boundary.com>
2015-06-24 07:53:55 -0700

    checkpoint of work in meter-client

--- boundary/meter_client.py
+++ boundary/meter_client.py
@@ -28,9 +28,9 @@ class MeterClient(ApiCli):
         self.rpc_host = 'localhost'
         self.rpc_port = 9192
         self.rpc_message = None
-        self.rpc_method = None
-        self.rpc_parameters = {}
-        self.MAX_LINE=1024
+        self.rpc_method = 'status'
+        self.rpc_parameters = None
+        self.MAX_LINE = 1024
 
     def getDescription(self):
         """
@@ -41,12 +41,13 @@ class MeterClient(ApiCli):
     def addArguments(self):
         self.addLoggingArgument()
         self.parser.add_argument('-m', '--method', dest='rpc_method', action='store', default='query_metric',
-                                 choices=['discovery', 'debug', 'status', 'query_metric', 'metric', 'event',
-                                          'get_system_info', 'get_service_listeners'],
+                                 choices=['debug', 'discovery', 'event', 'get_process_info', 'get_system_info',
+                                          'get_service_listeners', 'metric', 'query_metric', 'status'],
                                  help='Sets the RPC method to call on the meter')
         self.parser.add_argument('-r', '--rpc-host', dest='rpc_host', action='store', metavar='host',
                                  help='Sets the host or ip address to contact the meter on')
-        self.parser.add_argument('-p', '--parameters', dest='rpc_parameters', action='store', metavar='parameters',
+        self.parser.add_argument('-p', '--parameters', dest='rpc_parameters', action='store', nargs='*',
+                                 metavar='key=value',
                                  help='Sets the host or ip address to contact the meter on')
 
     def getArguments(self):
@@ -64,13 +65,57 @@ class MeterClient(ApiCli):
     def handleResults(self, result):
         print(self.rpc_data[0])
 
+    def parse_rpc_parameters(self):
+        p = {}
+
+        if self.rpc_parameters is not None:
+            for rpc_p in self.rpc_parameters:
+                sp = rpc_p.split('=')
+                print(sp)
+                p[sp[0]] = sp[1]
+                print(p)
+
+        return p
+
     def get_json(self):
         message = {}
 
         message['jsonrpc'] = '2.0'
         message['method'] = self.rpc_method
         message['id'] = 1
-        message['params'] = {'match': 'system.cpu.usage'}
+
+        # discovery - Retrieve the list of supported methods.
+        # debug - Enable/disable meter debug
+        # event - Push an event or an array of events
+        # get_process_info - Retrieve a list of processes matching a regex and return cpu/mem/disk info.
+        # get_system_info - Provide all the system metadata. cpu, mem, disk, network, etc.
+        # get_service_listeners - Retrieve a list of services listening on ports and protocols.
+        # metric - Push a metric sample or array of samples
+        # query_metric - Request internal metrics from the meter.
+        # status - Enable/disable periodic stats emission
+
+        if self.rpc_method == 'debug':
+            message['params'] = {'match': 'system.cpu.usage'}
+        elif self.rpc_method == 'discovery':
+            message['params'] = {}
+            pass
+        elif self.rpc_method == 'event':
+            pass
+        elif self.rpc_method == 'get_process_info':
+            message['params'] = self.parse_rpc_parameters()
+        elif self.rpc_method == 'get_system_info':
+            message['params'] = self.parse_rpc_parameters()
+        elif self.rpc_method == 'get_service_listeners':
+            message['params'] = self.parse_rpc_parameters()
+        elif self.rpc_method == 'metric':
+            pass
+        elif self.rpc_method == 'query_metric':
+            message['params'] = self.parse_rpc_parameters()
+        elif self.rpc_method == 'status':
+            message['params'] = self.parse_rpc_parameters()
+        else:
+            pass
+
         self.rpc_message = json.dumps(message)
         logging.debug('rpc_message: ' + self.rpc_message)
 
