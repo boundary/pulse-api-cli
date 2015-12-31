7652f8c6fc0ba4039b7058a64cbc8a169427e9eb
Gwartney <davidg@boundary.com>
2015-12-31 15:14:52 -0800

    Added test to excersize plugin get

--- tests/unit/boundary/plugin_get_test.py
+++ tests/unit/boundary/plugin_get_test.py
@@ -14,8 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import json
 from unittest import TestCase
+
 from boundary import PluginGet
+from cli_runner import CLIRunner
 from cli_test import CLITest
 
 
@@ -29,4 +32,28 @@ class PluginGetTest(TestCase):
 
     def test_cli_help(self):
         CLITest.check_cli_help(self, self.cli)
+
+    def test_create_metric(self):
+        runner = CLIRunner(PluginGet())
+
+        plugin_name = 'httpcheck'
+
+        get = runner.get_output(['-n', plugin_name])
+        plugin_get = json.loads(get)
+        plugin = plugin_get['result']
+
+        self.assertTrue('download' in plugin)
+        self.assertTrue('repoUrl' in plugin)
+        self.assertEqual(plugin_name, plugin['name'])
+        self.assertTrue('description' in plugin)
+        self.assertTrue('paramSchema' in plugin)
+        self.assertTrue('paramArray' in plugin)
+        self.assertTrue('postExtract' in plugin)
+        self.assertTrue('command' in plugin)
+        self.assertTrue('ignore' in plugin)
+        self.assertTrue('icon' in plugin)
+        self.assertTrue('dashboards' in plugin)
+        self.assertTrue('version' in plugin)
+        self.assertTrue('metrics' in plugin)
+        self.assertTrue('metricDefinitions' in plugin)
 
