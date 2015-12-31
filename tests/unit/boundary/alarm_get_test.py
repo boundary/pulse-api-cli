0639359bd0a9724bda806719ba1d534e975d69d1
Gwartney <davidg@boundary.com>
2015-12-30 19:00:24 -0800

    Add test to validate get

--- tests/unit/boundary/alarm_get_test.py
+++ tests/unit/boundary/alarm_get_test.py
@@ -16,9 +16,13 @@
 #
 
 from unittest import TestCase
+
 from boundary import AlarmGet
+from boundary import AlarmCreate
 from boundary import API
 from cli_test import CLITest
+from cli_runner import CLIRunner
+import json
 
 
 class AlarmGetTest(TestCase):
@@ -56,4 +60,29 @@ class AlarmGetTest(TestCase):
         self.assertEqual(alarm_create.aggregate, alarm_get.aggregate)
         self.assertEqual(alarm_create.operation, alarm_get.operation)
         self.assertEqual(alarm_create.threshold, alarm_get.threshold)
+
+    def test_get_alarm(self):
+        runner_create = CLIRunner(AlarmCreate())
+
+        create = runner_create.get_output(['-n', 'my-alarm', '-m', 'CPU', '-g', 'max', '-o', 'gt',
+                                    '-v', '0.50', '-r', '5 minutes'])
+        result_create = json.loads(create)
+        alarm_create = result_create['result']
+
+        runner_get = CLIRunner(AlarmGet())
+        get = runner_get.get_output(['-i', str(alarm_create['id'])])
+        result_get = json.loads(get)
+        alarm_get = result_get['result']
+
+        self.assertEqual(int(alarm_create['interval']), alarm_get['interval'])
+        self.assertEqual(alarm_create['actions'], alarm_get['actions'])
+        self.assertEqual(int(alarm_create['familyId']), int(alarm_get['familyId']))
+        self.assertFalse(alarm_create['isDisabled'], alarm_get['isDisabled'])
+        self.assertEqual(alarm_create['metricName'], alarm_get['metricName'])
+        self.assertEqual(alarm_create['name'], alarm_get['name'])
+        self.assertTrue(alarm_get['perHostNotify'])
+        self.assertEqual(alarm_create['triggerPredicate']['agg'], alarm_get['triggerPredicate']['agg'])
+        self.assertEqual(alarm_create['triggerPredicate']['op'], alarm_get['triggerPredicate']['op'])
+        self.assertEqual(alarm_create['triggerPredicate']['val'], alarm_get['triggerPredicate']['val'])
+        self.assertEqual(int(alarm_create['typeId']), int(alarm_get['typeId']))
 
