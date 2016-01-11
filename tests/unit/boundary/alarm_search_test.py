40a0bd6af11e4e9be9b2210d45c9015da762a296
Gwartney <davidg@boundary.com>
2016-01-11 15:06:14 -0800

    Fixed tests to clean up alarm definitions after tests

--- tests/unit/boundary/alarm_search_test.py
+++ tests/unit/boundary/alarm_search_test.py
@@ -16,7 +16,12 @@
 #
 
 from unittest import TestCase
+import json
+
 from cli_test import CLITest
+from cli_runner import CLIRunner
+from boundary import AlarmCreate
+from boundary import AlarmDelete
 from boundary import AlarmSearch
 
 
@@ -30,3 +35,42 @@ class AlarmSearchTest(TestCase):
 
     def test_cli_help(self):
         CLITest.check_cli_help(self, self.cli)
+
+    def test_search_alarm(self):
+        alarm_name = 'alarm_test' + CLITest.random_string(6)
+        metric_name = 'CPU'
+        aggregate = 'max'
+        op = 'gt'
+        value = 0.50
+        interval = '5 minutes'
+        note = CLITest.random_string(20)
+        enabled = True
+        runner_create = CLIRunner(AlarmCreate())
+
+        create = runner_create.get_output(['-n', alarm_name,
+                                           '-m', metric_name,
+                                           '-g', aggregate,
+                                           '-o', op,
+                                           '-v', str(value),
+                                           '-r', interval,
+                                           '-d', note,
+                                           '-x', str(enabled).lower()])
+
+        runner_search = CLIRunner(AlarmSearch())
+        search = runner_search.get_output(['-n', alarm_name])
+        result_search = json.loads(search)
+        alarm = result_search['result'][0]
+        self.assertEqual(int(300), alarm['interval'])
+        self.assertItemsEqual([], alarm['actions'])
+        self.assertEqual(1, int(alarm['familyId']))
+        self.assertFalse(False, alarm['isDisabled'])
+        self.assertEqual(metric_name, alarm['metricName'])
+        self.assertEqual(alarm_name, alarm['name'])
+        self.assertTrue(alarm['perHostNotify'])
+        self.assertEqual(aggregate, alarm['triggerPredicate']['agg'])
+        self.assertEqual(op, alarm['triggerPredicate']['op'])
+        self.assertEqual(value, alarm['triggerPredicate']['val'])
+        self.assertEqual(3, int(alarm['typeId']))
+
+        runner_delete = CLIRunner(AlarmDelete())
+        delete = runner_delete.get_output(['-i', str(alarm['id'])])
