9fe3b25054b6592bf2b13f98c1e4316ed9e2a15c
Gwartney <davidg@boundary.com>
2016-01-11 15:08:40 -0800

    Updated to remove alarm definitions after tests. But bug in API prevents clean up

--- tests/unit/boundary/alarm_update_test.py
+++ tests/unit/boundary/alarm_update_test.py
@@ -15,9 +15,15 @@
 # limitations under the License.
 #
 
+import json
 from unittest import TestCase
-from boundary import AlarmUpdate
+
 from boundary import API
+from boundary import AlarmCreate
+from boundary import AlarmDelete
+from boundary import AlarmGet
+from boundary import AlarmUpdate
+from cli_runner import CLIRunner
 from cli_test import CLITest
 
 
@@ -33,48 +39,108 @@ class AlarmUpdateTest(TestCase):
         CLITest.check_cli_help(self, self.cli)
 
     def test_api_call(self):
-        actions = [1693]
         aggregate = 'sum'
-        host_group_id = 1000
         interval = '1 minute'
         is_disabled = True
         metric_name = 'CPU'
         note = 'This is a note'
-        name = 'ALARM_CREATE_TEST'
+        alarm_name = 'ALARM_CREATE_TEST' + CLITest.random_string(4)
         operation = 'gt'
         per_host_modify = True
         threshold = '0.80'
-        alarm = self.api.alarm_create(actions=actions, aggregate=aggregate, interval=interval,
-                                      is_disabled=is_disabled, host_group_id=host_group_id, metric_name=metric_name,
-                                      name=name, note=note, operation=operation, per_host_modify=per_host_modify,
-                                      threshold=threshold)
+        alarm_create = self.api.alarm_create(aggregate=aggregate,
+                                             interval=interval,
+                                             is_disabled=is_disabled,
+                                             metric_name=metric_name,
+                                             name=alarm_name,
+                                             note=note,
+                                             operation=operation,
+                                             per_host_modify=per_host_modify,
+                                             threshold=threshold)
 
-        actions = [2000]
         aggregate = 'avg'
-        host_group_id = 2000
         interval = '1 hour'
         is_disabled = False
-        metric_name = 'SYSTEM.CPU.IOWAIT'
         metric_name = 'CPU'
         note = 'This is a updated note'
-        name = 'ALARM_UPDATE_TEST'
         operation = 'lt'
         per_host_modify = True
         threshold = '0.50'
 
-        # alarm = self.api.alarm_update(id=alarm.id, actions=actions, aggregate=aggregate, interval=interval,
-        #                               is_disabled=is_disabled, host_group_id=host_group_id, metric_name=metric_name,
-        #                               name=name, note=note, operation=operation, per_host_modify=per_host_modify,
-        #                               threshold=threshold)
-        #
-        # self.assertListEqual(actions, alarm.actions)
-        # self.assertEqual(aggregate, alarm.aggregate)
-        # self.assertEqual(3600, alarm.interval)
-        # self.assertEqual(is_disabled, alarm.is_disabled)
-        # self.assertEqual(host_group_id, alarm.host_group_id)
-        # self.assertEqual(metric_name, alarm.metric_name)
-        # self.assertEqual(name, alarm.name)
-        # self.assertEqual(note, alarm.note)
-        # self.assertEqual(operation, alarm.operation)
-        # self.assertEqual(per_host_modify, alarm.per_host_modify)
-        # self.assertEqual(threshold, alarm.threshold)
+        alarm_update = self.api.alarm_update(id=alarm_create.id,
+                                             aggregate=aggregate,
+                                             interval=interval,
+                                             is_disabled=is_disabled,
+                                             metric_name=metric_name,
+                                             name=alarm_name,
+                                             note=note,
+                                             operation=operation,
+                                             per_host_modify=per_host_modify,
+                                             threshold=threshold)
+
+        self.assertEqual(aggregate, alarm_update.aggregate)
+        self.assertEqual(3600, alarm_update.interval)
+        self.assertEqual(is_disabled, alarm_update.is_disabled)
+        self.assertEqual(metric_name, alarm_update.metric_name)
+        self.assertEqual(name, alarm_update.name)
+        self.assertEqual(note, alarm_update.note)
+        self.assertEqual(operation, alarm_update.operation)
+        self.assertEqual(per_host_modify, alarm_update.per_host_modify)
+        self.assertEqual(threshold, alarm_update.threshold)
+
+        self.api.alarm_delete(id=alarm_create.id)
+
+    def test_update_alarm(self):
+        alarm_name = 'my-alarm-' + CLITest.random_string(6)
+        metric_name = 'CPU'
+        note = CLITest.random_string(50)
+        aggregate = 'max'
+        op = 'gt'
+        value = 0.75
+        interval = '5 minutes'
+        is_disabled = True
+        runner_create = CLIRunner(AlarmCreate())
+        create = runner_create.get_output(['-n', alarm_name,
+                                           '-m', metric_name,
+                                           '-d', note,
+                                           '-g', aggregate,
+                                           '-o', op,
+                                           '-v', str(value),
+                                           '-r', str(interval),
+                                           '-x', str(is_disabled).lower()])
+        result_create = json.loads(create)
+        alarm_create = result_create['result']
+
+        note = CLITest.random_string(50)
+        aggregate = 'max'
+        op = 'gt'
+        value = 0.75
+        interval = '5 minutes'
+        is_disabled = False
+
+        runner_update = CLIRunner(AlarmUpdate())
+        update = runner_update.get_output(['-i', str(alarm_create['id']),
+                                           '-n', alarm_name,
+                                           '-m', metric_name,
+                                           '-d', note,
+                                           '-g', aggregate,
+                                           '-o', op,
+                                           '-v', str(value),
+                                           '-r', str(interval),
+                                           '-x', str(is_disabled).lower()])
+        result_update = json.loads(update)
+        alarm = result_update['result']
+
+        self.assertEqual(300, alarm['interval'])
+        self.assertEqual(1, alarm['familyId'])
+        self.assertFalse(is_disabled, alarm['isDisabled'])
+        self.assertEqual(metric_name, alarm['metricName'])
+        self.assertEqual(alarm_name, alarm['name'])
+        self.assertEqual(aggregate, alarm['triggerPredicate']['agg'])
+        self.assertEqual(op, alarm['triggerPredicate']['op'])
+        self.assertEqual(value, alarm['triggerPredicate']['val'])
+        self.assertEqual(3, int(alarm['typeId']))
+        self.assertEqual(note, int(alarm['note']))
+
+        runner_delete = CLIRunner(AlarmDelete())
+        delete = runner_delete.get_output(['-i', str(alarm['id'])])
